4f894900b34d68ad718d0e1780a0d390f17455cc
charityj26 <charity.joseph@student.moringaschool.com>
2023-07-02 17:14:01 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_25_145821) do
+ActiveRecord::Schema.define(version: 2023_07_02_145821) do
 
     create_table "students", force: :cascade do |t|
       t.string "first_name"
